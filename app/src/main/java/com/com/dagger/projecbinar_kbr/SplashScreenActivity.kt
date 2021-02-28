8e993694c72fda423601dd571b6e8ac38dc14542
defauser99 <defaihsanr@gmail.com>
2021-02-28 12:27:26 +0700

    update code from defa

--- app/src/main/java/com/com/dagger/projecbinar_kbr/SplashScreenActivity.kt
+++ app/src/main/java/com/com/dagger/projecbinar_kbr/SplashScreenActivity.kt
@@ -1,9 +1,11 @@
 package com.com.dagger.projecbinar_kbr
 
 import android.content.Intent
+import android.opengl.Visibility
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.com.dagger.projecbinar_kbr.databinding.ActivitySplashScreenBinding
@@ -20,6 +22,7 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(binding.root)
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
+            binding.progressBar.visibility = View.GONE
             finish()
         }, 2000)
         uploadImage("https://i.ibb.co/HC5ZPgD/splash-screen1.png")
