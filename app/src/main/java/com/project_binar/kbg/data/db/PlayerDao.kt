83625a4aa328d36eb5ea96049a23c0c91a4389c9
mfrizki1 <mfrizki1@gmail.com>
2021-03-01 20:59:03 +0700

    Added PlayerDao

--- app/src/main/java/com/project_binar/kbg/data/db/PlayerDao.kt
+++ app/src/main/java/com/project_binar/kbg/data/db/PlayerDao.kt
@@ -11,24 +11,27 @@ interface PlayerDao {
     fun addPlayer(player: Player): Long
 
     @Query("SELECT * FROM player WHERE username = :username AND password= :password LIMIT 1")
-    fun getListPlayer(username: String, password: String): Player?
+    fun loginPlayer(username: String, password: String): Player?
 
     @Query("UPDATE player SET win = :win WHERE id = :id")
-    fun updateWin(win: Int, id: Int) : Int
+    fun updateWin(win: Int, id: Int): Int
 
     @Query("UPDATE player SET lose = :lose WHERE id = :id")
-    fun updateLose(lose: Int,id: Int) : Int
+    fun updateLose(lose: Int, id: Int): Int
 
-    @Query("UPDATE player SET winrate = :winrate WHERE id = :id")
-    fun updateWinrate(winrate: Int, id: Int) : Int
+    @Query("UPDATE player SET winrate = :win/(:win+:lose) WHERE id = :id")
+    fun updateWinrate(winrate: Int, win: Int, lose: Int, id: Int): Int
 
     @Query("DELETE FROM player WHERE id = :id")
-    fun deleteNote(id: Int) : Int
+    fun deleteNote(id: Int): Int
 
     @Query("SELECT * FROM player WHERE id = :id")
     fun getSinglePlayer(id: Int): Player?
 
     @Query("UPDATE player SET nama = :nama WHERE id = :id")
-    fun updateNamePlayer(id: Int, nama: String):Int
+    fun updateNamePlayer(id: Int, nama: String): Int
+
+    @Query("SELECT * FROM player")
+    fun getAllPlayer():MutableList<Player>
 
 }
