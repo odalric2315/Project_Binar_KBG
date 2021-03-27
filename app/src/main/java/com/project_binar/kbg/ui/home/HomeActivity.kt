136ec18bf3aca6f0bbf248064fea92cdb50a4bf4
defauser99 <defaihsanr@gmail.com>
2021-03-27 09:41:52 +0700

    add feature show password from defa

--- app/src/main/java/com/project_binar/kbg/ui/home/HomeActivity.kt
+++ app/src/main/java/com/project_binar/kbg/ui/home/HomeActivity.kt
@@ -120,7 +120,7 @@ class HomeActivity : AppCompatActivity(){
     private fun toLogin() {
         suitPrefs.clearSharePref()
         val intent = Intent(this, LoginActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
         finish()
     }
