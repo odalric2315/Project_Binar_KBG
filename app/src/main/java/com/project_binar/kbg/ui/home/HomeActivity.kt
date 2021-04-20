b9d8c6498901916cd9f110b68da725f4a5632a6a
Reynolds <michaelreynoldk@gmail.com>
2021-04-20 12:31:19 +0700

    minor improvment

--- app/src/main/java/com/project_binar/kbg/ui/home/HomeActivity.kt
+++ app/src/main/java/com/project_binar/kbg/ui/home/HomeActivity.kt
@@ -151,6 +151,7 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun toLogin() {
+        stopBackgroundSound()
         suitPrefs.clearSharePref()
         val intent = Intent(this, LoginActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
